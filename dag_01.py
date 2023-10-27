99ad11c68cad3c1f6dc306e47fa9006eec03fd1c
David Courtey <david_courtey@yahoo.com>
2023-10-27 17:30:45 +0200

    Create dag_01.py

---
+++ dag_01.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+default_args = {
+    'owner': 'yiyu',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    default_args=default_args,
+    dag_id="dag_with_cron_expression_v04",
+    start_date=datetime(2023, 10, 24),
+    schedule_interval='0 3 * * Tue-Fri'
+) as dag:
+    task1 = KubernetesPodOperator(
+    name="hello-dry-run",
+    image="debian",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    task_id="dry_run_demo",
+    do_xcom_push=True,
+)
+    task1
